302889e6f4d9b02b99e4e2c60bd2a4cd8f390a17
Alex Suraci <suraci.alex@gmail.com>
2015-11-02 10:52:58 -0800

    clone with --recursive

--- README.sh
+++ README.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # step 1
-git clone https://github.com/vito/dot-nvim.git ~/.config/nvim
+git clone https://github.com/vito/dot-nvim.git ~/.config/nvim --recursive
 
 # there is no step 2
