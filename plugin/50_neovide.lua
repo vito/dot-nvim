8e8988f9bec13f23376aa1dd6bcf0b80fd1f482a
Alex Suraci <suraci.alex@gmail.com>
2026-04-06 00:45:20 -0400

    add neovide config
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

---
+++ plugin/50_neovide.lua
@@ -0,0 +1,15 @@
+if not vim.g.neovide then return end
+
+vim.o.guifont = 'Iosevka Term SS08:h12'
+
+-- support ctrl+- and ctrl+= for zooming
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set("n", "<C-=>", function()
+  change_scale_factor(1.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1/1.25)
+end)
