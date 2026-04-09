c87657b2470f784f3f5a450a1952b980b5b043ff
Alex Suraci <suraci.alex@gmail.com>
2026-04-09 17:01:43 -0400

    neovide: add mac paste keybinds
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- plugin/50_neovide.lua
+++ plugin/50_neovide.lua
@@ -15,3 +15,13 @@ vim.keymap.set("n", "<C-->", function()
 end)
 
 vim.g.neovide_floating_corner_radius = 0.0
+
+if vim.fn.has('macunix') == 1 then
+  local function save() vim.cmd.write() end
+  local function copy() vim.cmd([[normal! "+y]]) end
+  local function paste() vim.api.nvim_paste(vim.fn.getreg("+"), true, -1) end
+
+  vim.keymap.set({ "n", "i", "v" }, "<D-s>", save, { desc = "Save" })
+  vim.keymap.set("v", "<D-c>", copy, { silent = true, desc = "Copy" })
+  vim.keymap.set({ "n", "i", "v", "c", "t" }, "<D-v>", paste, { silent = true, desc = "Paste" })
+end
